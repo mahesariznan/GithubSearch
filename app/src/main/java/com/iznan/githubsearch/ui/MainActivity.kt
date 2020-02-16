a1d2db6ca8831364f31e18d32ab1524aa30de4e1
Iznan <mahesar.iznan.abdillah@gmail.com>
2020-02-16 19:32:08 +0700

    Initial commit

---
+++ app/src/main/java/com/iznan/githubsearch/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.iznan.githubsearch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iznan.githubsearch.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
